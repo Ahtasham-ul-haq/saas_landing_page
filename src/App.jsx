0553a32d8c710db0abd6964d6a347bfaf5e3b3cc
Ahtasham-ul-Haq <ahtasham1802@gmail.com>
2024-09-24 23:51:25 +0500

    implementing Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./sections/Header"
 
 const App = () => {
     return (
-        <h1 className={`text-3xl font-bold underline`}>Hello World!</h1>
+        <main className="overflow-hidden">
+            <Header />
+        </main>
     )
 }
 export default App
