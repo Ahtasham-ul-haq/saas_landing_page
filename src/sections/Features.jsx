831ec1a6d7b45fa5a56371d65975baf1d1b536e8
Ahtasham-ul-Haq <ahtasham1802@gmail.com>
2024-10-05 21:42:47 +0500

    implement Features.jsx

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -32,7 +32,7 @@ const Features = () => {
                                 <div className='flex items-center justify-center mx-auto mb-3 border-2 border-s2 rounded-full hover:border-s4 transition-all duration-500 shadow-500 size-20'>
                                     <img src={icon} alt={title} className='size-17/20 object-contain z-20' />
                                 </div>
-                                <h3></h3>
+                                <h3 className='relative z-2 max-w-36 mx-auto my-0 base-small text-center uppercase'>{title}</h3>
                             </li>
 
                         ))}
